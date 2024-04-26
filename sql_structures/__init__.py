f9c5b245bbdb6f4f66ad32c7560c3a22281a9493
Andreé Robles <andreerm14@gmail.com>
2024-04-26 00:15:19 -0600

    crud Doctor

--- sql_structures/__init__.py
+++ sql_structures/__init__.py
@@ -1,1 +1,4 @@
 from .usuario import SqlDataBase_usuarios
+from .inventario_farmacia import inventarioFarmacia
+from .manager import Manager
+from .doctor import Doctor
--- views/main_view.py
+++ views/main_view.py
@@ -1,13 +1,12 @@
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import *
-#import sql_structures
-import pandas as pdf
-import webbrowser
-import pdfrw
-from datetime import date
-#from encrypt import *
-from reportlab.pdfgen import canvas
-from datetime import datetime
+import sql_structures
+import pandas as pd
+from reportlab.lib.pagesizes import legal, landscape
+from reportlab.lib import colors
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+import os
+from encrypt import *
 
 
 class VentanaPrincipal(QMainWindow):
@@ -25,8 +24,17 @@ class VentanaPrincipal(QMainWindow):
         self.btn_modulo_receta.clicked.connect(self.show_page_receta)
         self.btn_agregar_farmacia.clicked.connect(self.show_page_agregar_i)
         self.btn_actualizar_farmacia.clicked.connect(self.show_page_actualizar_i)
+        # botones doctor
+        self.btn_agregarDoctor.clicked.connect(self.show_page_agregar_doc)
+        self.btn_actualizarDoc.clicked.connect(self.show_page_actualizar_doc)
+        self.agregarDoc_btn.clicked.connect(self.registrar_doctor)
+        self.actualizarDoc_btn.clicked.connect(self.actualizar_doctor)
+        self.tabla_doctores.cellClicked.connect(self.click_tabla_doctor)
+        self.btn_eliminar_doctor.clicked.connect(self.eliminar_doctor)
+        self.cargar_tabla_doc_btn.clicked.connect(self.carga_tabla_doctor)
 
 
+    # Funcionalidad de botones
 
     def inicio_sesion(self):
         self.frame_modulos.show()
@@ -48,3 +56,89 @@ class VentanaPrincipal(QMainWindow):
 
     def show_page_actualizar_i(self):
         self.stackedWidget.setCurrentWidget(self.page_actualizar_inventario)
+
+    def show_page_agregar_doc(self):
+        self.stackedWidget.setCurrentWidget(self.page_agregar_doctor)
+
+    def show_page_actualizar_doc(self):
+        self.stackedWidget.setCurrentWidget(self.page_actualizar_doctor)
+
+    # Doctor
+    def registrar_doctor(self):
+        try:
+            doctor = sql_structures.Doctor(self.nombreDoc_le.text(),
+                                           self.especialidadDoc_le.text(),
+                                           self.costCita_ln.text())
+            doctor.management('ag_doctor')
+            self.nombreDoc_le.clear()
+            self.especialidadDoc_le.clear()
+            self.costCita_ln.clear()
+            QMessageBox.about(self, 'Aviso', 'Agregado correctamente!')
+        except Exception as e:
+            print(e)
+            QMessageBox.about(self, 'Aviso', 'Error de agregado!')
+
+    def actualizar_doctor(self):
+        try:
+            doctor = sql_structures.Doctor('',
+                                           '',
+                                           '',
+                                           self.datoCambiarDoc_box.currentText(),
+                                           self.nuevoValorDoc_le.text(),
+                                           self.idActualizarDoc_le.text())
+            doctor.management('up_doctor')
+            self.idActualizarDoc_le.clear()
+            self.nuevoValorDoc_le.clear()
+            QMessageBox.about(self, 'Aviso', 'Actualizado correctamente!')
+        except Exception as e:
+            print(e)
+            QMessageBox.about(self, 'Aviso', 'Error al actualizar!')
+
+    def eliminar_doctor(self):
+        try:
+            doctor = sql_structures.Doctor('',
+                                           '',
+                                           '',
+                                           '',
+                                           '',
+                                           self.id_c)
+            doctor.management('del_doctor')
+            QMessageBox.about(self, 'Aviso', 'Se elimino con exito!')
+        except Exception as e:
+            print(e)
+            QMessageBox.about(self, 'Aviso', 'Eliminacion fallida!')
+        self.carga_tabla_doctor()
+
+    def click_tabla_doctor(self, row, column):
+        print('1')
+        manager = sql_structures.Manager()
+        print('1')
+        item = self.tabla_doctores.item(row, column)
+        value = item.text()
+        print('2')
+        columns_ingreso = ['id', 'nombre', 'especialidad', 'cobroCita']
+        header_item = self.tabla_doctores.horizontalHeaderItem(column)
+        column_name = header_item.text()
+        print('3')
+
+        if column_name == 'ID':
+            self.id_c = manager.get('doctor', columns_ingreso, value, 'id')
+        elif column_name == 'Nombre':
+            self.id_c = manager.get('doctor', columns_ingreso, value, 'nombre')
+        elif column_name == 'Especialidad':
+            self.id_c = manager.get('doctor', columns_ingreso, value, 'especialidad')
+        elif column_name == 'Cita':
+            self.id_c = manager.get('doctor', columns_ingreso, value, 'cobroCita')
+
+    def carga_tabla_doctor(self):
+        try:
+            mana = sql_structures.Manager()
+            dato = mana.print_table('doctor')
+            self.tabla_doctores.setRowCount(len(dato))
+            for i in range(len(dato)):
+                self.tabla_doctores.setItem(i, 0, QTableWidgetItem(str(dato[i][0])))
+                self.tabla_doctores.setItem(i, 1, QTableWidgetItem(str(dato[i][1])))
+                self.tabla_doctores.setItem(i, 2, QTableWidgetItem(str(dato[i][2])))
+                self.tabla_doctores.setItem(i, 3, QTableWidgetItem(str(dato[i][3])))
+        except Exception as e:
+            print(e)
