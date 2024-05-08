0613ba3eee0ef579c440b305e43904dcaa66e291
Andreé Robles <andreerm14@gmail.com>
2024-05-07 22:41:24 -0600

    CRUD FARMACIA

--- views/main_view.py
+++ views/main_view.py
@@ -24,6 +24,7 @@ class VentanaPrincipal(QMainWindow):
         self.btn_modulo_receta.clicked.connect(self.show_page_receta)
         self.btn_agregar_farmacia.clicked.connect(self.show_page_agregar_i)
         self.btn_actualizar_farmacia.clicked.connect(self.show_page_actualizar_i)
+
         # botones doctor
         self.btn_agregarDoctor.clicked.connect(self.show_page_agregar_doc)
         self.btn_actualizarDoc.clicked.connect(self.show_page_actualizar_doc)
@@ -32,6 +33,13 @@ class VentanaPrincipal(QMainWindow):
         self.tabla_doctores.cellClicked.connect(self.click_tabla_doctor)
         self.btn_eliminar_doctor.clicked.connect(self.eliminar_doctor)
         self.cargar_tabla_doc_btn.clicked.connect(self.carga_tabla_doctor)
+
+        # botones inventario farmacias
+        self.agMedicamento_btn.clicked.connect(self.registrar_medicamento)
+        self.actualizarMedicamento_btn.clicked.connect(self.actualizar_medicamento)
+        self.cargarInventarioMed_btn.clicked.connect(self.carga_tabla_medicamento)
+        self.btn_eliminarMedicamento.clicked.connect(self.eliminar_medicamento)
+        self.tablaFarmacia.cellClicked.connect(self.click_tabla_medicamento)
 
 
     # Funcionalidad de botones
@@ -136,5 +144,90 @@ class VentanaPrincipal(QMainWindow):
                 self.tabla_doctores.setItem(i, 1, QTableWidgetItem(str(dato[i][1])))
                 self.tabla_doctores.setItem(i, 2, QTableWidgetItem(str(dato[i][2])))
                 self.tabla_doctores.setItem(i, 3, QTableWidgetItem(str(dato[i][3])))
+        except Exception as e:
+            print(e)
+
+    # Inventario Medicamentos
+    def registrar_medicamento(self):
+        try:
+            medicina = sql_structures.InventarioFarmacia(self.nombreMedicamento_le.text(),
+                                                         self.cantMedicamento_ln.text(),
+                                                         self.descMedicamento_le.text(),
+                                                         self.precioMedicamento_le.text(),
+                                                         self.fechaMedicamento_le.text())
+            medicina.management('ag_inventarioFarmacia')
+            self.nombreMedicamento_le.clear()
+            self.cantMedicamento_ln.clear()
+            self.descMedicamento_le.clear()
+            self.precioMedicamento_le.clear()
+            self.fechaMedicamento_le.clear()
+            QMessageBox.about(self, 'Aviso', 'Agregado correctamente!')
+        except Exception as e:
+            print(e)
+            QMessageBox.about(self, 'Aviso', 'Error de agregado!')
+
+    def actualizar_medicamento(self):
+        try:
+            medicamento = sql_structures.InventarioFarmacia('',
+                                                            '',
+                                                            '',
+                                                            '',
+                                                            '',
+                                                            self.datoCambiarMed_box.currentText(),
+                                                            self.nuevoValorMed_le.text(),
+                                                            self.idActualizarMed_le.text())
+            medicamento.management('up_inventarioFarmacia')
+            QMessageBox.about(self, 'Aviso', 'Actualizado correctamente!')
+        except Exception as e:
+            print(e)
+            QMessageBox.about(self, 'Aviso', 'Error al actualizar!')
+
+    def eliminar_medicamento(self):
+        try:
+            medicina = sql_structures.InventarioFarmacia('',
+                                                         '',
+                                                         '',
+                                                         '',
+                                                         '',
+                                                         '',
+                                                         '',
+                                                         self.id_c)
+            medicina.management('del_inventarioFarmacia')
+            QMessageBox.about(self, 'Aviso', 'Se elimino con exito!')
+        except Exception as e:
+            print(e)
+            QMessageBox.about(self, 'Aviso', 'Eliminacion fallida!')
+        self.carga_tabla_doctor()
+
+    def click_tabla_medicamento(self, row, column):
+        manager = sql_structures.Manager()
+        item = self.tablaFarmacia.item(row, column)
+        value = item.text()
+        columns_ingreso = ['id', 'nombre', 'cantidad', 'descripcion', 'precio', 'fecha_de_vencimiento']
+        header_item = self.tablaFarmacia.horizontalHeaderItem(column)
+        column_name = header_item.text()
+
+        if column_name == 'Nombre':
+            self.id_c = manager.get('inventariofarmacia', columns_ingreso, value, 'nombre')
+        elif column_name == 'Cantidad':
+            self.id_c = manager.get('inventariofarmacia', columns_ingreso, value, 'cantidad')
+        elif column_name == 'Descripcion':
+            self.id_c = manager.get('inventariofarmacia', columns_ingreso, value, 'descripcion')
+        elif column_name == 'Precio':
+            self.id_c = manager.get('inventariofarmacia', columns_ingreso, value, 'precio')
+        elif column_name == 'Fecha_Ven':
+            self.id_c = manager.get('inventariofarmacia', columns_ingreso, value, 'fecha_ven')
+
+    def carga_tabla_medicamento(self):
+        try:
+            mana = sql_structures.Manager()
+            dato = mana.print_table('inventariofarmacia')
+            self.tablaFarmacia.setRowCount(len(dato))
+            for i in range(len(dato)):
+                self.tablaFarmacia.setItem(i, 0, QTableWidgetItem(str(dato[i][1])))
+                self.tablaFarmacia.setItem(i, 1, QTableWidgetItem(str(dato[i][2])))
+                self.tablaFarmacia.setItem(i, 2, QTableWidgetItem(str(dato[i][3])))
+                self.tablaFarmacia.setItem(i, 3, QTableWidgetItem(str(dato[i][4])))
+                self.tablaFarmacia.setItem(i, 4, QTableWidgetItem(str(dato[i][5])))
         except Exception as e:
             print(e)
